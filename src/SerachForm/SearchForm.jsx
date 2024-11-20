ad196531ca97c245878f10b539cc562a4cb7aa93
charuzain <charuabhizain@gmail.com>
2024-11-19 22:22:09 -0500

    Add searchform compoenent

--- src/SerachForm/SearchForm.jsx
+++ src/SerachForm/SearchForm.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import './SearchForm.scss'
 
 const SearchForm = () => {
+  const [search, setSearchTerm] = useState('')
+  
+  const formSubmitHandler = (event) => {
+    event.preventDefault()
+    console.log(event.target.value)
+  }
+
+  const inputHandler = (e) => {
+    setSearchTerm(e.target.value);
+    
+  }
   return (
-    <div>SearchForm</div>
+    <section className="hero">
+      <h1 className="hero__title">Unsplash Images</h1>
+      <form onSubmit={formSubmitHandler} className="form">
+        <input type="text" name="search" placeholder="cat" onInput={inputHandler} value={search} className="form__input"/>
+        <input type="submit" value="Search" className="form__button" />
+      </form>
+    </section>
   )
 }
 
